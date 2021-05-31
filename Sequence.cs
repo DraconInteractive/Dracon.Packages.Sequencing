f677abd138bb0874112fcc1a25acf79dd049d6df
Dracon <petermarkcarey@gmail.com>
2021-05-31 15:27:37 +0800

    init

---
+++ Sequence.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DI_Sequences
+{
+    public class Sequence : MonoBehaviour
+    {
+        public bool running;
+        [SerializeField] public List<IAction> actions = new List<IAction>();
+    }
+
+    public interface IAction
+    {
+        void Begin();
+        void Complete();
+    }
+
+    [Serializable]
+    public class DebugAction : IAction
+    {
+        [HideInInspector]
+        public string name;
+        public bool running;
+
+        public string message;
+
+        public DebugAction()
+        {
+            name = "Debug Action";
+        }
+
+        public void Begin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Serializable]
+    public class MoveTransformAction : IAction
+    {
+        [HideInInspector]
+        public string name;
+        public bool running;
+
+        public Transform target;
+        public Transform endPosition;
+
+        public MoveTransformAction()
+        {
+            name = "Move Transform Action";
+        }
+
+        public void Begin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SequenceManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI_Sequences
+{
+    class SequenceManager
+    {
+        public List<Sequence> sequences = new List<Sequence>();
+    }
+}
