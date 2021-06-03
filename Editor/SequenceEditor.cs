a22d2d521dc8b7de8b67e806d21f9cbe8221ff85
Dracon <petermarkcarey@gmail.com>
2021-06-03 22:29:17 +0800

    upgrades!

--- Editor/SequenceEditor.cs
+++ Editor/SequenceEditor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEditor;
+using UnityEditor.IMGUI.Controls;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -27,38 +28,14 @@ namespace DI_Sequences
             {
                 Rect selectionRect = new Rect(position.x, position.y + propHeight + EditorGUIUtility.standardVerticalSpacing, position.width, 50);
 
-                List<string> options = new List<string>() { "Select Action" };
-                Array.ForEach(GetInheritedClasses(typeof(SequenceAction)), x => options.Add(x.ToString()));
-
-                int i = EditorGUI.Popup(selectionRect, "Create Sequence: ", 0, options.ToArray());
-
-                if (i != 0)
+                if (GUI.Button(selectionRect, new GUIContent("Show"), EditorStyles.toolbarButton))
                 {
-                    SerializedProperty newAction = prop.GetArrayElementAtIndex(prop.arraySize++);
-                    newAction.managedReferenceValue = GetInstance(options[i]);
-                    /*
-                    switch (type)
-                    {
-                        case Sequence.ActionType.Debug:
-                            newAction.managedReferenceValue = new DebugAction();
-                            break;
-                    }*/
-                }
+                    var dropdown = new ActionsDropdown(new AdvancedDropdownState());
+                    dropdown.Show(selectionRect);
+                }  
             }
 
             EditorGUI.EndProperty();
-        }
-
-        public object GetInstance(string strFullyQualifiedName)
-        {
-            Type t = Type.GetType(strFullyQualifiedName);
-            return Activator.CreateInstance(t);
-        }
-
-        Type[] GetInheritedClasses(Type MyType)
-        {
-            //if you want the abstract classes drop the !TheType.IsAbstract but it is probably to instance so its a good idea to keep it.
-            return Assembly.GetAssembly(MyType).GetTypes().Where(TheType => TheType.IsClass && !TheType.IsAbstract && TheType.IsSubclassOf(MyType)).ToArray();
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
@@ -66,6 +43,33 @@ namespace DI_Sequences
             SerializedProperty prop = property.FindPropertyRelative("actions");
 
             return EditorGUI.GetPropertyHeight(prop, true) + EditorGUIUtility.standardVerticalSpacing + (prop.isExpanded ? EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing : 0);
+        }
+
+        class ActionsDropdown : AdvancedDropdown
+        {
+            public SerializedProperty prop;
+            public ActionsDropdown(AdvancedDropdownState state) : base(state)
+            { }
+
+            protected override AdvancedDropdownItem BuildRoot()
+            {
+                var root = new AdvancedDropdownItem("Main");
+
+                foreach (var v in TypeCache.GetTypesDerivedFrom<SequenceAction>().ToArray())
+                {
+                    root.AddChild(new AdvancedDropdownItem(v.FullName));
+                }
+                return root;
+            }
+
+            protected override void ItemSelected(AdvancedDropdownItem item)
+            {
+                base.ItemSelected(item);
+
+                SerializedProperty newAction = prop.GetArrayElementAtIndex(prop.arraySize++);
+                Type t = Type.GetType(item.name);
+                newAction.managedReferenceValue = Activator.CreateInstance(t);
+            }
         }
     }
 }
